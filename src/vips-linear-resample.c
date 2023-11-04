44ac7f0e5cebc565934c34beafcb557fb977c86b
damianfral <huevofritopamojarpan@gmail.com>
2023-11-04 21:51:30 +0000

    Refactor, extract some functions.

--- src/vips-linear-resample.c
+++ src/vips-linear-resample.c
@@ -1,14 +1,43 @@
 #include <vips/vips.h>
 
+double get_scale_factor(int width, int height, VipsImage *img) {
+  double scale;
+  double scaleV = (double)height / vips_image_get_height(img);
+  double scaleH = (double)width / vips_image_get_width(img);
+
+  if (scaleV < scaleH) {
+    scale = scaleV;
+  } else {
+    scale = scaleH;
+  }
+  return scale;
+}
+
+VipsImage *scale_image(int width, int height, VipsImage *img) {
+  double scale = get_scale_factor(width, height, img);
+  VipsImage *new_image = vips_image_new();
+  vips_resize(img, &new_image, scale, NULL);
+  return new_image;
+}
+
+void export_image(VipsImage *img, char *filepath) {
+  char avif_options[64] = "[Q=66,subsample_mode=VIPS_FOREIGN_SUBSAMPLE_OFF]";
+  char avif_output[256] = "";
+  strcat(avif_output, filepath);
+  strcat(avif_output, avif_options);
+  vips_colourspace(img, &img, VIPS_INTERPRETATION_sRGB, NULL);
+  // Save the resized image as AVIF with no chroma subsampling
+  if (vips_image_write_to_file(img, avif_output, NULL) != 0) {
+    vips_error_exit("Error saving AVIF output image");
+  }
+}
+
 int main() {
   if (VIPS_INIT(NULL) != 0) {
     vips_error_exit("Error initializing VIPS");
   }
 
   VipsImage *input_image = vips_image_new_from_file("test.jpg", NULL);
-  VipsImage *resized_image_4K = vips_image_new();
-  VipsImage *resized_image_1K = vips_image_new();
-  VipsImage *resized_thumbnail = vips_image_new();
 
   if (input_image == NULL) {
     vips_error_exit("Error loading input image");
@@ -16,52 +45,19 @@ int main() {
 
   vips_colourspace(input_image, &input_image, VIPS_INTERPRETATION_scRGB, NULL);
 
-  double scaleV = 2160.0 / vips_image_get_height(input_image);
-  double scaleH = 3840.0 / vips_image_get_width(input_image);
-  double scale;
-
-  if (scaleV < scaleH) {
-    scale = scaleV;
-  } else {
-    scale = scaleH;
-  }
-
-  vips_resize(input_image, &resized_image_4K, scale, NULL);
-  vips_resize(resized_image_4K, &resized_image_1K, 0.5, NULL);
-  vips_resize(resized_image_1K, &resized_thumbnail, 0.25, NULL);
-
-  vips_colourspace(resized_image_4K, &resized_image_4K,
-                   VIPS_INTERPRETATION_sRGB, NULL);
-  vips_colourspace(resized_image_1K, &resized_image_1K,
-                   VIPS_INTERPRETATION_sRGB, NULL);
-  vips_colourspace(resized_thumbnail, &resized_thumbnail,
-                   VIPS_INTERPRETATION_sRGB, NULL);
+  VipsImage *resized_image_4K = scale_image(3840, 2160, input_image);
+  VipsImage *resized_image_1K = scale_image(1920, 1080, input_image);
+  VipsImage *resized_image_thumb = scale_image(480, 270, input_image);
 
   // Save the resized image as AVIF with no chroma subsampling
-  if (vips_image_write_to_file(
-          resized_image_4K,
-          "output-4K.avif[Q=60,subsample_mode=VIPS_FOREIGN_SUBSAMPLE_OFF]",
-          NULL) != 0) {
-    vips_error_exit("Error saving AVIF output image");
-  }
-
-  if (vips_image_write_to_file(
-          resized_image_1K,
-          "output-1K.avif[Q=50,subsample_mode=VIPS_FOREIGN_SUBSAMPLE_OFF]",
-          NULL) != 0) {
-    vips_error_exit("Error saving AVIF output image");
-  }
-
-  if (vips_image_write_to_file(resized_thumbnail, "output-thumb.avif[Q=50]",
-                               NULL) != 0) {
-    vips_error_exit("Error saving AVIF output image");
-  }
-
+  export_image(resized_image_4K, "output-4K.avif");
+  export_image(resized_image_1K, "output-HD.avif");
+  export_image(resized_image_thumb, "output-thumb.avif");
   // Clean up
   g_object_unref(input_image);
   g_object_unref(resized_image_4K);
   g_object_unref(resized_image_1K);
-  g_object_unref(resized_thumbnail);
+  g_object_unref(resized_image_thumb);
   vips_shutdown();
 
   return 0;

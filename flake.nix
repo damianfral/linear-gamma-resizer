303f919de04a9964ef04d280e31a77315e248a16
damianfral <huevofritopamojarpan@gmail.com>
2023-11-04 19:18:23 +0000

    Initial commit.

---
+++ flake.nix
@@ -0,0 +1,65 @@
+{
+  description = "Image Resizer using libvips";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+    nix-filter.url = "github:numtide/nix-filter";
+    pre-commit-hooks.url = "github:cachix/pre-commit-hooks.nix";
+    pre-commit-hooks.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    nix-filter,
+    pre-commit-hooks,
+    ...
+  }: let
+    pkgsFor = system: import nixpkgs {inherit system;};
+    filteredSrc = nix-filter.lib {
+      root = ./.;
+      include = [
+        "src/"
+      ];
+    };
+  in
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = pkgsFor system;
+        precommitCheck = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            actionlint.enable = true;
+            hlint.enable = true;
+            hpack.enable = true;
+            markdownlint.enable = true;
+            nil.enable = true;
+            alejandra.enable = true;
+            ormolu.enable = true;
+            clang-format.enable = true;
+          };
+        };
+      in {
+        packages.default = pkgs.clangStdenv.mkDerivation {
+          name = "vips-linear-resample";
+          src = filteredSrc;
+          buildInputs = [pkgs.vips pkgs.glibc pkgs.glib pkgs.pkg-config];
+          meta = with pkgs.lib; {
+            description = "C program to resize images using libvips";
+            license = licenses.mit;
+          };
+          buildPhase = ''
+            $CC $(pkg-config vips --cflags --libs) -lpthread -lvips -O3 -o vips-linear-resample src/vips-linear-resample.c
+          '';
+          installPhase = ''
+            mkdir -p $out/bin
+            cp vips-linear-resample $out/bin
+          '';
+        };
+        devShells.default = (pkgs.mkShell.override {stdenv = pkgs.clangStdenv;}) {
+          packages = with pkgs; [vips glib pkg-config clang-tools];
+          shellHook = precommitCheck.shellHook;
+        };
+      }
+    );
+}
---
+++ src/vips-linear-resample.c
@@ -0,0 +1,55 @@
+#include <vips/vips.h>
+
+int main() {
+    if (VIPS_INIT(NULL) != 0) { vips_error_exit("Error initializing VIPS"); }
+
+    VipsImage *input_image = vips_image_new_from_file("test.jpg", NULL);
+    VipsImage *resized_image_4K = vips_image_new();
+    VipsImage *resized_image_1K = vips_image_new();
+    VipsImage *resized_thumbnail = vips_image_new();
+
+    if (input_image == NULL) { vips_error_exit("Error loading input image"); }
+
+    vips_colourspace(input_image, &input_image, VIPS_INTERPRETATION_scRGB, NULL);
+
+    double scaleV = 2160.0 / vips_image_get_height(input_image);
+    double scaleH = 3840.0 / vips_image_get_width(input_image);
+    double scale;
+
+    if (scaleV < scaleH) { scale = scaleV ; }
+    else { scale = scaleH; }
+
+    vips_resize(input_image, &resized_image_4K, scale, NULL);
+    vips_resize(resized_image_4K, &resized_image_1K, 0.5 , NULL);
+    vips_resize(resized_image_1K, &resized_thumbnail, 0.25 , NULL);
+    
+    vips_colourspace(resized_image_4K, &resized_image_4K, VIPS_INTERPRETATION_sRGB, NULL);
+    vips_colourspace(resized_image_1K, &resized_image_1K, VIPS_INTERPRETATION_sRGB, NULL);
+    vips_colourspace(resized_thumbnail, &resized_thumbnail, VIPS_INTERPRETATION_sRGB, NULL);
+
+    // Save the resized image as AVIF with no chroma subsampling
+    if (vips_image_write_to_file(
+         resized_image_4K, 
+         "output-4K.avif[Q=60,subsample_mode=VIPS_FOREIGN_SUBSAMPLE_OFF]",
+         NULL) != 0) 
+    { vips_error_exit("Error saving AVIF output image"); }
+
+    if (vips_image_write_to_file(
+         resized_image_1K, 
+         "output-1K.avif[Q=50,subsample_mode=VIPS_FOREIGN_SUBSAMPLE_OFF]", 
+         NULL) != 0) 
+    { vips_error_exit("Error saving AVIF output image"); }
+
+    if (vips_image_write_to_file(
+         resized_thumbnail, "output-thumb.avif[Q=50]", NULL) != 0) 
+    { vips_error_exit("Error saving AVIF output image"); }
+
+    // Clean up
+    g_object_unref(input_image);
+    g_object_unref(resized_image_4K);
+    g_object_unref(resized_image_1K);
+    g_object_unref(resized_thumbnail);
+    vips_shutdown();
+
+    return 0;
+}
